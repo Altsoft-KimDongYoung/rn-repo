649a0606bcd7a6ca8047910bb330f37dc061e656
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-01-06 06:15:30 +0900

    test

--- sum.ts
+++ sum.ts
@@ -1,3 +1,14 @@
 import { useQuery } from "react-query";
 
 export const sum = (a: number, b: number, c: number): number => a + b + c;
+export const sumAsync = async (
+  a: number,
+  b: number,
+  c: number
+): Promise<number> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(a + b + c);
+    }, 1000);
+  });
+};
