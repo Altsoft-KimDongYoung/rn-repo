eebee294478460a5bf29860d1449ea7e1f7da95d
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-02-03 06:10:51 +0900

    MAINGOOD

--- sum.ts
+++ sum.ts
@@ -10,7 +10,7 @@ export const sumAsync = async (
     }, 1000);
   });
 };
-export const c2221111onsole222222222LogMain = (
+export const c2221111onsole222222222LogMainGOOD = (
   a: number,
   b: number,
   c: number,
